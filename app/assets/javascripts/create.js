c314564e21db4f6a8fffdefd33e6ddbab58e9463
YukoOkayasu <yukookayasu@yukonoMacBook-Pro.local>
2019-01-27 17:15:41 +0900

    Add-incremental-search

--- app/assets/javascripts/create.js
+++ app/assets/javascripts/create.js
@@ -1,59 +1,61 @@
-$(function() {
-  function buildHTML(message){
-    if (message.image !== null) {
-      img = `<image src="${message.image}" class="lower-message__image" >`
-    } else {
-      img = ''
-    }
-    if (message.content !== null) {
-      text = `<p class="lower-message__content">
-                ${message.content}
-              </p>`
-    }
-    var html = `<div class="message">
-                  <div class="upper-message">
-                    <div class="upper-message__user-name">
-                      ${message.user_name}
+document.addEventListener('turbolinks:load', function() {
+  $(function() {
+    function buildHTML(message){
+      if (message.image !== null) {
+        img = `<image src="${message.image}" class="lower-message__image" >`
+      } else {
+        img = ''
+      }
+      if (message.content !== null) {
+        text = `<p class="lower-message__content">
+                  ${message.content}
+                </p>`
+      }
+      var html = `<div class="message">
+                    <div class="upper-message">
+                      <div class="upper-message__user-name">
+                        ${message.user_name}
+                      </div>
+                      <div class="upper-message__date">
+                        ${message.date}
+                      </div>
                     </div>
-                    <div class="upper-message__date">
-                      ${message.date}
+                    <div class="lower-meesage">
+                      ${text}
+                      ${img}
                     </div>
-                  </div>
-                  <div class="lower-meesage">
-                    ${text}
-                    ${img}
-                  </div>
-                </div>`
-    return html;
-  }
-  $('#form__message').on('submit', function(e) {
-    //HTMlでの送信をキャンセル
-    e.preventDefault();
-    //送信
-    var formData = new FormData(this);
-    var url = $(this).attr('action')
-    $.ajax({
-      type: "POST",
-      url: url,
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
+                  </div>`
+      return html;
+    }
+    $('#new_message').on('submit', function(e) {
+      //HTMlでの送信をキャンセル
+      e.preventDefault();
+      //送信
+      var formData = new FormData(this);
+      var url = $(this).attr('action')
+      $.ajax({
+        type: "POST",
+        url: url,
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
+      //通信成功時の処理
+      .done(function(data) {
+        var html = buildHTML(data);
+        $('.messages').append(html)
+        $('#new_message')[0].reset()
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      })
+      //通信失敗時の処理
+      .fail(function() {
+        alert('error');
+      })
+      //常にdisabledを消して送信ボタンを押せるようにする
+      .always(function() {
+        $('.form__submit').prop('disabled', false)
+      })
     })
-    //通信成功時の処理
-    .done(function(data) {
-      var html = buildHTML(data);
-      $('.messages').append(html)
-      $('#form__message')[0].reset()
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-    })
-    //通信失敗時の処理
-    .fail(function() {
-      alert('error');
-    })
-    //常にdisabledを消して送信ボタンを押せるようにする
-    .always(function() {
-      $('.form__submit').prop('disabled', false)
-    })
-  })
+  });
 });
---
+++ app/assets/javascripts/search.js
@@ -0,0 +1,58 @@
+document.addEventListener('turbolinks:load', function() {
+  $(function() {
+
+  var searchList = $("#user-search-result")
+  function appendUser(user) {
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</a>
+                </div>`
+    searchList.append(html);
+  }
+
+  var memberList = $("#chat-group-users")
+  function removeUser(id, name) {
+    var html =`<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${id}'>
+                <input name='group[user_ids][]' type='hidden' value=${id}>
+                <p class='chat-group-user__name'>${name}</p>
+                <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+              </div>`
+    memberList.append(html);
+  }
+
+    $("#user-search-field").on("keyup", function() {
+      var input = $(this).val();
+      console.log(self);
+
+      $.ajax({
+        type: 'GET',
+        url: '/users',
+        data: { keyword: input },
+        dataType: 'json'
+      })
+
+      .done(function(users) {
+        var self = $()
+        $("#user-search-result").empty();
+        $(self).hide();
+          users.forEach(function(user) {
+            appendUser(user);
+          });
+      })
+      .fail(function() {
+        alert('名前検索に失敗しました');
+      });
+    });
+
+    $(document).on("click", ".chat-group-user__btn--add", function() {
+      $(this).parent().remove();
+      var id = $(this).data('user-id')
+      var name = $(this).data('user-name')
+        removeUser(id, name)
+    });
+
+    $(document).on("click", ".js-remove-btn", function() {
+      $(this).parent().remove();
+    });
+  });
+});
